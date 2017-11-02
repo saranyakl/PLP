cd20bb41daa6ec81888ad02c68648adc152ef60c
Saranya <klsaranya108@gmail.com>
2017-11-01 20:35:28 -0400

    Errors corrected

--- cop5556fa17/TypeCheckTest.java
+++ cop5556fa17/TypeCheckTest.java
@@ -112,5 +112,7 @@ public class TypeCheckTest {
 	 typeCheck(input);
 	 }
 
+	
+	 
 
 }
--- cop5556fa17/TypeCheckVisitor.java
+++ cop5556fa17/TypeCheckVisitor.java
@@ -72,9 +72,6 @@ public class TypeCheckVisitor implements ASTVisitor {
 			throws Exception {
 		if(symbolTable.containsKey(declaration_Variable.name))
 			throw new SemanticException(declaration_Variable.firstToken, "From Declaration_Variable, name already exists");
-		else {
-			symbolTable.put(declaration_Variable.name, declaration_Variable);
-		}
 		declaration_Variable.ntype = TypeUtils.getType(declaration_Variable.firstToken);
 		if(declaration_Variable.e!=null) {
 			Type et = (Type) declaration_Variable.e.visit(this, null);
@@ -82,52 +79,45 @@ public class TypeCheckVisitor implements ASTVisitor {
 				throw new SemanticException(declaration_Variable.firstToken, "From declaration_Variable, type not matching");
 			}
 		}
-		else
-			throw new SemanticException(declaration_Variable.firstToken, "From declaration_Variable, expression not found");
+		symbolTable.put(declaration_Variable.name, declaration_Variable);
 		return declaration_Variable.ntype;
 	}
 
 	@Override
 	public Object visitExpression_Binary(Expression_Binary expression_Binary,
 			Object arg) throws Exception {	
-		if(expression_Binary.e0!=null && expression_Binary.e1!=null) {
+//		if(expression_Binary.e0!=null && expression_Binary.e1!=null) {
 			Type e0t = (Type) expression_Binary.e0.visit(this, null);
 			Type e1t = (Type) expression_Binary.e1.visit(this, null);
-			if(e0t==e1t) {
-				switch(expression_Binary.op) {
-					case OP_EQ:case OP_NEQ:
-						expression_Binary.ntype = Type.BOOLEAN;
-						break;
-					case OP_GE:case OP_GT:case OP_LE:case OP_LT:
-						if(e0t==Type.INTEGER)
-							expression_Binary.ntype = Type.BOOLEAN;
-						else
-							throw new SemanticException(expression_Binary.firstToken,"expression_Binary, require failed");
-						break;
-					case OP_AND:case OP_OR:
-						if(e0t==Type.INTEGER || e0t==Type.BOOLEAN) {
-							expression_Binary.ntype = e0t;
-						}
-						else
-							throw new SemanticException(expression_Binary.firstToken,"expression_Binary, require failed");
-						break;
-					case OP_DIV:case OP_MINUS:case OP_MOD:case OP_PLUS:case OP_POWER:case OP_TIMES:
-						if(e0t==Type.INTEGER) {
-							expression_Binary.ntype = Type.INTEGER;
-						}
-						else
-							throw new SemanticException(expression_Binary.firstToken,"expression_Binary, require failed");
-						break;
-					default:
-//						expression_Binary.ntype = Type.NONE;
+			switch(expression_Binary.op) {
+			case OP_EQ:case OP_NEQ:
+				expression_Binary.ntype = Type.BOOLEAN;
+				break;
+			case OP_GE:case OP_GT:case OP_LE:case OP_LT:
+				if(e0t==Type.INTEGER)
+					expression_Binary.ntype = Type.BOOLEAN;
+				break;
+			case OP_AND:case OP_OR:
+				if(e0t==Type.INTEGER || e0t==Type.BOOLEAN) {
+					expression_Binary.ntype = e0t;
 				}
+				break;
+			case OP_DIV:case OP_MINUS:case OP_MOD:case OP_PLUS:case OP_POWER:case OP_TIMES:
+				if(e0t==Type.INTEGER) {
+					expression_Binary.ntype = Type.INTEGER;
+				}
+				break;
+			default:
+				break;
+//				expression_Binary.ntype = Type.NONE;
+		}
+			if(e0t==e1t) {
+				
 			}
 			else {
 				throw new SemanticException(expression_Binary.firstToken,"expression_Binary, require failed");
 			}
-		}
-		else
-			throw new SemanticException(expression_Binary.firstToken, "From expression_Binary, expressions not found");
+//		}
 		if(expression_Binary.ntype == Type.NONE)
 			throw new SemanticException(expression_Binary.firstToken,"expression_Binary, require statement failed");
 		return expression_Binary.ntype;
@@ -136,15 +126,13 @@ public class TypeCheckVisitor implements ASTVisitor {
 	@Override
 	public Object visitExpression_Unary(Expression_Unary expression_Unary,
 			Object arg) throws Exception {
-		if(expression_Unary.e!=null) {
+//		if(expression_Unary.e!=null) {
 			Type et = (Type) expression_Unary.e.visit(this, null);
 			switch(expression_Unary.op) {
 			case OP_EXCL:
 				if(et==Type.BOOLEAN || et==Type.INTEGER) {
 					expression_Unary.ntype = et;
 				}
-				else
-					throw new SemanticException(expression_Unary.firstToken,"expression_Unary, require failed");
 				break;
 			case OP_MINUS: case OP_PLUS:
 				if(et==Type.INTEGER) {
@@ -154,9 +142,7 @@ public class TypeCheckVisitor implements ASTVisitor {
 			default:
 				break;
 			}
-		}
-		else
-			throw new SemanticException(expression_Unary.firstToken, "From expression_Unary, expression not found");
+//		}
 		if(expression_Unary.ntype==Type.NONE)
 			throw new SemanticException(expression_Unary.firstToken, "From Expression_Unary, require failed");
 		return expression_Unary.ntype;
@@ -164,19 +150,18 @@ public class TypeCheckVisitor implements ASTVisitor {
 
 	@Override
 	public Object visitIndex(Index index, Object arg) throws Exception {
-		if(index.e0!=null && index.e1!=null) {
+//		if(index.e0!=null && index.e1!=null) {
 			Type e0t = (Type) index.e0.visit(this, null);
 			Type e1t = (Type) index.e1.visit(this, null);
-			if(e0t==e1t && e0t==Type.INTEGER) {
+			if(e0t==Type.INTEGER && e1t==Type.INTEGER) {
 				index.setCartesian(!(index.e0.firstToken.kind == Kind.KW_r && index.e1.firstToken.kind == Kind.KW_A));
 			}
 			else
 				throw new SemanticException(index.firstToken,"From Index, require failed");
 			
-		}
-		else
-			throw new SemanticException(index.firstToken, "From index, expressions not found");
-		return index.ntype;
+//		}
+//		return index.ntype;
+		return index.isCartesian();
 	}
 
 	@Override
@@ -185,25 +170,19 @@ public class TypeCheckVisitor implements ASTVisitor {
 			throws Exception {
 		if(expression_PixelSelector.index!=null) {
 			expression_PixelSelector.index.visit(this, null);
-			if(symbolTable.containsKey(expression_PixelSelector.name)) {
-				Type nametype = symbolTable.get(expression_PixelSelector.name).ntype;
-				if(nametype==Type.IMAGE)
-					expression_PixelSelector.ntype = Type.INTEGER;
-				else ;
-			}
-			else
-				throw new SemanticException(expression_PixelSelector.firstToken,"From expression_PixelSelector, name doesnot exist");
 		}
-		else {
-			if(symbolTable.containsKey(expression_PixelSelector.name)) {
-				Type nametype = symbolTable.get(expression_PixelSelector.name).ntype;
-				expression_PixelSelector.ntype = nametype;
-			}
-			else
-				throw new SemanticException(expression_PixelSelector.firstToken,"From expression_PixelSelector, name doesnot exist");
+		if(!symbolTable.containsKey(expression_PixelSelector.name)) {
+			throw new SemanticException(expression_PixelSelector.firstToken,"From expression_PixelSelector, name doesnot exist");
 		}
+		Type nametype = symbolTable.get(expression_PixelSelector.name).ntype;
+		if(nametype==Type.IMAGE)
+			expression_PixelSelector.ntype = Type.INTEGER;
+		else if(expression_PixelSelector.index==null) {
+			expression_PixelSelector.ntype = nametype;
+		}
+		
 		if(expression_PixelSelector.ntype==Type.NONE)
-			throw new SemanticException(expression_PixelSelector.firstToken, "from expression_PixelSelector, require failed");
+			throw new SemanticException(expression_PixelSelector.firstToken, "From expression_PixelSelector, require failed");
 		return expression_PixelSelector.ntype;
 	}
 
@@ -211,48 +190,45 @@ public class TypeCheckVisitor implements ASTVisitor {
 	public Object visitExpression_Conditional(
 			Expression_Conditional expression_Conditional, Object arg)
 			throws Exception {
-		if(expression_Conditional.condition!=null && expression_Conditional.falseExpression!=null 
-				&& expression_Conditional.trueExpression!=null) {
+//		if(expression_Conditional.condition!=null && expression_Conditional.falseExpression!=null 
+//				&& expression_Conditional.trueExpression!=null) {
 			Type ec = (Type) expression_Conditional.condition.visit(this, null);
 			Type et = (Type) expression_Conditional.trueExpression.visit(this, null);
 			Type ef = (Type) expression_Conditional.falseExpression.visit(this, null);
+			expression_Conditional.ntype = et;
 			if(ec==Type.BOOLEAN && et==ef) {
-				expression_Conditional.ntype = et;
+				
 			}
 			else
 				throw new SemanticException(expression_Conditional.firstToken,"From expression_Conditional, require failed");
-		}
-		else
-			throw new SemanticException(expression_Conditional.firstToken, "expression_conditional, expressions not found");
+//		}
 		return expression_Conditional.ntype;
 	}
 
 	@Override
 	public Object visitDeclaration_Image(Declaration_Image declaration_Image,
 			Object arg) throws Exception {
+		if(symbolTable.containsKey(declaration_Image.name))
+			throw new SemanticException(declaration_Image.firstToken, "From declaration_Image, name already exists");
+		
 		if(declaration_Image.xSize!=null) {
+			Type xt = (Type) declaration_Image.xSize.visit(this, null);
+			Type yt = Type.NONE;
 			if(declaration_Image.ySize==null) {
 				throw new SemanticException(declaration_Image.firstToken, "From declaration_Image, require failed");
 			}
-		}
-		else {
-			if(declaration_Image.ySize!=null)
-				throw new SemanticException(declaration_Image.firstToken, "From declaration_Image, require failed");
-		}
-		if(declaration_Image.xSize!=null && declaration_Image.ySize!=null && declaration_Image.source!=null) {
-			Type xt = (Type) declaration_Image.xSize.visit(this, null);
-			Type yt = (Type) declaration_Image.ySize.visit(this, null);
-			declaration_Image.source.visit(this, null);
-			if(symbolTable.containsKey(declaration_Image.name))
-				throw new SemanticException(declaration_Image.firstToken, "From declaration_Image, name already exists");
 			else {
-				symbolTable.put(declaration_Image.name, declaration_Image);
+				yt = (Type) declaration_Image.ySize.visit(this, null);
 			}
 			if(xt!=Type.INTEGER && yt!=Type.INTEGER) {
 				throw new SemanticException(declaration_Image.firstToken, "From declaration_Image, require failed");
 			}
-			declaration_Image.ntype = Type.IMAGE;
 		}
+		if(declaration_Image.source!=null) {
+			declaration_Image.source.visit(this, null);
+		}
+		declaration_Image.ntype = Type.IMAGE;
+		symbolTable.put(declaration_Image.name, declaration_Image);
 		return declaration_Image.ntype;
 	}
 
@@ -286,16 +262,15 @@ public class TypeCheckVisitor implements ASTVisitor {
 	public Object visitSource_CommandLineParam(
 			Source_CommandLineParam source_CommandLineParam, Object arg)
 			throws Exception {
-		if(source_CommandLineParam.paramNum!=null) {
+//		if(source_CommandLineParam.paramNum!=null) {
 			Type et = (Type) source_CommandLineParam.paramNum.visit(this, null);
+			source_CommandLineParam.ntype=et;
 			if(et==Type.INTEGER) {
-				source_CommandLineParam.ntype=et;
+				
 			}
 			else
 				throw new SemanticException(source_CommandLineParam.firstToken,"From source_CommandLineParam, require failed");
-		}
-		else
-			throw new SemanticException(source_CommandLineParam.firstToken, "from source_commandlineparam, expression not found");
+//		}
 		return source_CommandLineParam.ntype;
 	}
 
@@ -303,7 +278,10 @@ public class TypeCheckVisitor implements ASTVisitor {
 	public Object visitSource_Ident(Source_Ident source_Ident, Object arg)
 			throws Exception {
 		if(symbolTable.containsKey(source_Ident.name)) {
-			source_Ident.ntype = symbolTable.get(source_Ident.name).ntype;
+			if(symbolTable.get(source_Ident.name)!=null)
+				source_Ident.ntype = symbolTable.get(source_Ident.name).ntype;
+			else
+				throw new SemanticException(source_Ident.firstToken, "From source_Ident, no declaration found");
 		}
 		//Symbol table error
 		else {
@@ -318,19 +296,16 @@ public class TypeCheckVisitor implements ASTVisitor {
 	public Object visitDeclaration_SourceSink(
 			Declaration_SourceSink declaration_SourceSink, Object arg)
 			throws Exception {
-		if(declaration_SourceSink.source!=null) {
+		if(symbolTable.containsKey(declaration_SourceSink.name))
+			throw new SemanticException(declaration_SourceSink.firstToken, "From declaration_Image, name already exists");
+		
+//		if(declaration_SourceSink.source!=null) {
 			Type st = (Type) declaration_SourceSink.source.visit(this, null);
-			if(symbolTable.containsKey(declaration_SourceSink.name))
-				throw new SemanticException(declaration_SourceSink.firstToken, "From declaration_Image, name already exists");
-			else {
-				symbolTable.put(declaration_SourceSink.name, declaration_SourceSink);
-			}
+			symbolTable.put(declaration_SourceSink.name, declaration_SourceSink);
 			declaration_SourceSink.ntype = TypeUtils.getType(declaration_SourceSink.firstToken);
 			if(st!=declaration_SourceSink.ntype)
 				throw new SemanticException(declaration_SourceSink.firstToken, "From declaration_SourceSink, require failed");
-		}
-		else
-			throw new SemanticException(declaration_SourceSink.firstToken, "From declaration_sourcesink, Source not found");
+//		}
 		return declaration_SourceSink.ntype;
 	}
 
@@ -345,15 +320,15 @@ public class TypeCheckVisitor implements ASTVisitor {
 	public Object visitExpression_FunctionAppWithExprArg(
 			Expression_FunctionAppWithExprArg expression_FunctionAppWithExprArg,
 			Object arg) throws Exception {
-		if(expression_FunctionAppWithExprArg.arg!=null) {
+//		if(expression_FunctionAppWithExprArg.arg!=null) {
 			Type et = (Type) expression_FunctionAppWithExprArg.arg.visit(this, null);
+			
 			if(et==Type.INTEGER)
-				expression_FunctionAppWithExprArg.ntype = Type.INTEGER;
+				;
 			else
 				throw new SemanticException(expression_FunctionAppWithExprArg.firstToken,"From expression_FunctionAppWithExprArg, require failed");
-		}
-		else
-			throw new SemanticException(expression_FunctionAppWithExprArg.firstToken, "From expression_FunctionAppWithExprArg, expression not found");
+//		}
+		expression_FunctionAppWithExprArg.ntype = Type.INTEGER;
 		return expression_FunctionAppWithExprArg.ntype;
 	}
 
@@ -363,9 +338,6 @@ public class TypeCheckVisitor implements ASTVisitor {
 			Object arg) throws Exception {
 		if(expression_FunctionAppWithIndexArg.arg!=null)
 			expression_FunctionAppWithIndexArg.arg.visit(this, null);
-		else
-			throw new SemanticException(expression_FunctionAppWithIndexArg.firstToken, 
-					"From expression_FuncAppwithIndexArg, no Index found");
 		expression_FunctionAppWithIndexArg.ntype = Type.INTEGER;
 		return expression_FunctionAppWithIndexArg.ntype;
 	}
@@ -381,11 +353,13 @@ public class TypeCheckVisitor implements ASTVisitor {
 	@Override
 	public Object visitStatement_Out(Statement_Out statement_Out, Object arg)
 			throws Exception {
-		if(statement_Out.sink!=null) {
+		if(!symbolTable.containsKey(statement_Out.name))
+			throw new SemanticException(statement_Out.firstToken, "From statement_out, name not found");
+//		if(statement_Out.sink!=null) {
 			Type sinkt = (Type) statement_Out.sink.visit(this, null);
+			Type namet = (Type) symbolTable.get(statement_Out.name).ntype;
+			statement_Out.setDec(symbolTable.get(statement_Out.name));
 			if(symbolTable.get(statement_Out.name)!=null) {
-				Type namet = (Type) symbolTable.get(statement_Out.name).ntype;
-				statement_Out.setDec(symbolTable.get(statement_Out.name));
 				if(!(((namet==Type.INTEGER || namet == Type.BOOLEAN) && sinkt==Type.SCREEN) 
 						|| (namet==Type.IMAGE && (sinkt==Type.FILE || sinkt==Type.SCREEN)))) {
 					throw new SemanticException(statement_Out.firstToken,"From statement_Out, require statement failed");
@@ -394,59 +368,53 @@ public class TypeCheckVisitor implements ASTVisitor {
 			else
 				throw new SemanticException(statement_Out.firstToken, "From statement_out, name not found");
 			
-		}
-		else
-			throw new SemanticException(statement_Out.firstToken, "Ffrom ststatement_out, sink not found");
+//		}
 		return statement_Out.ntype;
 	}
 
 	@Override
 	public Object visitStatement_In(Statement_In statement_In, Object arg)
 			throws Exception {
-		if(statement_In.source!=null) {
+//		if(statement_In.source!=null) {
+		
 			Type st = (Type) statement_In.source.visit(this, null);
+			statement_In.setDec(symbolTable.get(statement_In.name));
 			if(symbolTable.get(statement_In.name)!=null && symbolTable.get(statement_In.name).ntype==st) {
-				statement_In.setDec(symbolTable.get(statement_In.name));
+				
 			}
 			else
 				throw new SemanticException(statement_In.firstToken,"from statement_In, Require statement failed");
-		}
-		else
-			throw new SemanticException(statement_In.firstToken, "From statement_in, source not found");
+//		}
 		return statement_In.ntype;
 	}
 
 	@Override
 	public Object visitStatement_Assign(Statement_Assign statement_Assign,
 			Object arg) throws Exception {
-		if(statement_Assign.lhs!=null && statement_Assign.e!=null) {
+//		if(statement_Assign.lhs!=null && statement_Assign.e!=null) {
 			Type et = (Type) statement_Assign.e.visit(this, null);
 			Type it = (Type) statement_Assign.lhs.visit(this, null);
 			if(et!=it) {
 				throw new SemanticException(statement_Assign.firstToken,"From statement_Assign, type mismatch");
 			}
-			statement_Assign.setCartesian(statement_Assign.lhs.isCartesian());
-		}
-		else
-			throw new SemanticException(statement_Assign.firstToken, "From statement_Assign, lhs or expression not found");
+//		}
+		statement_Assign.setCartesian(statement_Assign.lhs.isCartesian());
 		return statement_Assign.ntype;
 	}
 
 	@Override
 	public Object visitLHS(LHS lhs, Object arg) throws Exception {
 		if(lhs.index!=null) {
-			lhs.index.visit(this, null);
-			if(symbolTable.containsKey(lhs.name)) {
-				lhs.setDec(symbolTable.get(lhs.name));
-			}
-			//Symbol table error
-			else
-				throw new SemanticException(lhs.firstToken,"From Lhs, No name found");
-			lhs.ntype = lhs.getDec().ntype;
-			lhs.setCartesian(lhs.index.isCartesian());
+			boolean b = (boolean) lhs.index.visit(this, null);
+			lhs.setCartesian(b);
 		}
+		if(symbolTable.containsKey(lhs.name)) {
+		}
+		//Symbol table error
 		else
-			throw new SemanticException(lhs.firstToken, "From lhs: No index found");
+			throw new SemanticException(lhs.firstToken,"From Lhs, No name found");
+		lhs.setDec(symbolTable.get(lhs.name));
+		lhs.ntype = lhs.getDec().ntype;
 		return lhs.ntype;
 	}
 
